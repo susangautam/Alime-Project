c8ba301a8f9a9058659a9791f1249be36641bd07
susangautam <off.susangautam@gmail.com>
2024-12-22 12:58:55 +0545

    Some changes in pages

--- src/Components/Pages.jsx
+++ src/Components/Pages.jsx
@@ -20,19 +20,19 @@ function Pages() {
          Video
         </li>
       </ul>
-      <div className="grid grid-rows-3 grid-flow-col gap-3 columns-lg justify-center m-40 ">
+      <div className="grid grid-rows-3 grid-flow-col gap-x-3  justify-center m-40 ">
         
-         <div><img src="3.jpg" className="h-44"></img></div> 
-         <div><img src="7.jpg"></img></div> 
-         <div><img src="37.jpg"></img></div> 
-         <div><img src="4.jpg" className="h-80"></img></div> 
-         <div><img src="9.jpg"></img></div> 
-         <div><img src="5.jpg"></img></div> 
-         <div><img src="5.jpg" className="h-40"></img></div> 
-         <div><img src="8.jpg"></img></div> 
-         <div><img src="36.jpg" className="h-80"></img></div> 
-         <div><img src="6.jpg"></img></div>  
-         <div><img src="10.jpg" className="h-96"></img></div> 
+         <div ><img src="3.jpg" className="h-44 order-1"></img></div> 
+         <div><img src="7.jpg" className="order-2"></img></div> 
+         <div><img src="37.jpg" className="order-3"></img></div> 
+         <div><img src="4.jpg" className="h-80 order-4"></img></div> 
+         <div className="-mt-28"><img src="9.jpg" className="order-5"></img></div> 
+         <div><img src="5.jpg" className="order-6"></img></div> 
+         <div><img src="5.jpg" className="h-40 order-7"></img></div> 
+         <div><img src="8.jpg" className="order-8"></img></div> 
+         <div><img src="36.jpg" className="h-80 order-9"></img></div> 
+         <div><img src="6.jpg" className="order-10"></img></div>  
+         <div><img src="10.jpg"  className="h-96 order-last"></img></div> 
           
         
       </div>
